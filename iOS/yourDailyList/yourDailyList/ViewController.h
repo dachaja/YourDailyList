8e8dbf6d120916299dcb223622463c28885485ab
dachaja <pettitbusiness@gmail.com>
2014-08-14 22:35:29 -0700

    add AFNetworking from cocoa pods

---
+++ iOS/yourDailyList/yourDailyList/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  yourDailyList
+//
+//  Created by Ahyeon Jo Pettit on 2014-08-14.
+//  Copyright (c) 2014 Anna Jo Pettit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *btnLogin;
+
+@end
